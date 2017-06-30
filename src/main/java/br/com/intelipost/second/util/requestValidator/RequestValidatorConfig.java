33af24e345a54202b5647e63540ece213a6b3569
lucastex <lucas@blanq.com.br>
2017-06-30 19:52:08 -0300

    refactor - creating the correct session strategy from properties

--- src/main/java/br/com/intelipost/second/util/requestValidator/RequestValidatorConfig.java
+++ src/main/java/br/com/intelipost/second/util/requestValidator/RequestValidatorConfig.java
@@ -1,5 +1,6 @@
 package br.com.intelipost.second.util.requestValidator;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,18 +10,38 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RequestValidatorConfig {
 
-    @Bean("webRequestValidator")
-    public RequestValidator getSessionRequestValidator() {
-        return new SessionRequestValidator();
-    }
+    @Value("${ip.session.strategy}")
+    private String sessionStategy;
 
-    //@Bean("webRequestValidator")
-    public RequestValidator getCookieRequestValidator() {
-        return new CookieRequestValidator();
+    @Bean("webRequestValidator")
+    public RequestValidator getCookieRequestValidator() throws IllegalAccessException, InstantiationException {
+        return SessionStrategy.valueOf(sessionStategy).getClazz().newInstance();
     }
 
     @Bean("restRequestValidator")
     public RequestValidator getRestRequestValidator() {
         return new HeaderRequestValidator();
+    }
+
+    private enum SessionStrategy {
+
+        cookie("cookie", CookieRequestValidator.class),
+        http("http", SessionRequestValidator.class);
+
+        private String value;
+        private Class clazz;
+
+        SessionStrategy(String value, Class requestValidatorClass) {
+            this.value = value;
+            this.clazz = requestValidatorClass;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public Class<RequestValidator> getClazz() {
+            return clazz;
+        }
     }
 }
