c4c94e766f02daeb9f2c8f6f3d647c5106b01108
lucastex <lucas@blanq.com.br>
2017-07-01 11:25:01 -0300

    add - profiles retrieval by user (to use on rest controller)

--- src/main/java/br/com/intelipost/second/services/UserService.java
+++ src/main/java/br/com/intelipost/second/services/UserService.java
@@ -1,5 +1,7 @@
 package br.com.intelipost.second.services;
 
+import br.com.intelipost.second.domain.Profile;
+import br.com.intelipost.second.repositories.ProfileRepository;
 import br.com.intelipost.second.repositories.UserRepository;
 import br.com.intelipost.second.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +18,18 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private ProfileRepository profileRepository;
+
     public List<User> listUsers() {
         return userRepository.listUsers();
     }
 
     public User getUser(Long id) {
         return userRepository.findById(id);
+    }
+
+    public List<Profile> getProfilesByUser(Long userId) {
+        return profileRepository.getProfilesByUser(userId);
     }
 }
