15a4b8a77df34e39060e6bbe64684d94550ada1d
lucastex <lucas@blanq.com.br>
2017-07-01 17:14:40 -0300

    add - readonly transaction on linkrepository

--- src/main/java/br/com/intelipost/second/repositories/LinkRepository.java
+++ src/main/java/br/com/intelipost/second/repositories/LinkRepository.java
@@ -6,6 +6,7 @@ import br.com.intelipost.second.util.mappers.LinkRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,6 +22,7 @@ public class LinkRepository {
     @Autowired
     private LinkRowMapper linkRowMapper;
 
+    @Transactional(readOnly = true)
     public List<Link> findLinksByToken(String token) {
         return jdbcTemplate.query("select l.* from link l, link_profile lp, user_profile up, user_token ut where l.id = lp.link_id and lp.profile_id = up.profile_id and up.user_id = ut.user_id and ut.hash = ?;", new Object[]{ token }, linkRowMapper);
     }
