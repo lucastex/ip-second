f571390f7cb69377cfac5fe1f7b16dade90fd45e
lucastex <lucas@blanq.com.br>
2017-07-01 10:59:09 -0300

    add - enhanced url mappings with base mapping

--- src/main/java/br/com/intelipost/second/controllers/rest/UserController.java
+++ src/main/java/br/com/intelipost/second/controllers/rest/UserController.java
@@ -1,5 +1,6 @@
 package br.com.intelipost.second.controllers.rest;
 
+import br.com.intelipost.second.domain.Profile;
 import br.com.intelipost.second.domain.User;
 import br.com.intelipost.second.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,19 +15,25 @@ import java.util.List;
  * Created by lucastex on 30/06/17.
  */
 @RestController
+@RequestMapping(value = "/api/users/")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/api/users", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public List<User> listUsers() {
         return userService.listUsers();
     }
 
-    @RequestMapping(value = "/api/users/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public User getUser(@PathVariable Long id) {
         return userService.getUser(id);
+    }
+
+    @RequestMapping(value = "/{id}/profiles", method = RequestMethod.GET)
+    public List<Profile> getUserProfiles(@PathVariable Long id) {
+        return userService.getProfilesByUser(id);
     }
 
 }
